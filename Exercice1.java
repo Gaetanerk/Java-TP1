9925adc12fcd7b43146216ce91a5eeb33f9c6f75
gaetanerk <gaetan.turben@outlook.com>
2023-12-11 13:47:37 +0100

    tp1exercice1

---
+++ Exercice1.java
@@ -0,0 +1,13 @@
+package exercices.tp1;
+
+public class Exercice1 {
+    public static void main(String[] args) {
+        int note1 = 12;
+        int note2 = 15;
+        int note3 = 7;
+        int moyenne = (note1 + note2 + note3) / 3;
+
+        System.out.println("La moyenne des 3 notes est : " + moyenne);
+    }
+}
+
